1baa604052a435b1196fbaec331774917398aa66
georgva0 <vgeorg@yahoo.com>
2022-09-01 20:48:35 +0100

    Add extra row to homepage list

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
 
   useEffect(() => {
     fetch(
-      `https://sheets.googleapis.com/v4/spreadsheets/13YqOJHhi2lrt4hFMQ4FdZ6rv2N_Gf9oyMg2fdObTn4E/values/data!A2:B40?key=${process.env.REACT_APP_GOOGLE_CLOUD_KEY}`
+      `https://sheets.googleapis.com/v4/spreadsheets/13YqOJHhi2lrt4hFMQ4FdZ6rv2N_Gf9oyMg2fdObTn4E/values/data!A2:B41?key=${process.env.REACT_APP_GOOGLE_CLOUD_KEY}`
     )
       .then((response) => response.json())
       .then((data) => setScores(data.values));
